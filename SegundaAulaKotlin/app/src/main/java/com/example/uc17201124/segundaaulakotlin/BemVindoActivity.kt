22ef5293ef3b7ce0d3db2c5a8c93934bbeb265df
MATHEUS RODRIGUES <UC17201124@SLAB.UCB.BR>
2018-10-17 11:01:51 -0300

    Exercicio login senha

--- SegundaAulaKotlin/app/src/main/java/com/example/uc17201124/segundaaulakotlin/BemVindoActivity.kt
+++ SegundaAulaKotlin/app/src/main/java/com/example/uc17201124/segundaaulakotlin/BemVindoActivity.kt
@@ -1,12 +1,19 @@
 package com.example.uc17201124.segundaaulakotlin
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_bem_vindo.*
 
 class BemVindoActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bem_vindo)
+
+        btnProsseguir.setOnClickListener {
+            var intent = Intent(this, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
